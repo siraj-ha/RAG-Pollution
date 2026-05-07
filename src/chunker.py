fa5dd840b957465a433231c6890d58077d1b3a47
Mohamed Siraj Eddine Hamoudi <sirajhamoudi885@gmail.com>
2026-05-07 10:35:03 +0100

    some changes

--- src/chunker.py
+++ src/chunker.py
@@ -1,5 +1,5 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-def chunk_documents(documents, chunk_size=512, chunk_overlap=64):
+def chunk_documents(documents, chunk_size=800, chunk_overlap=100):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap,
